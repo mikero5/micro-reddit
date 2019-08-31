a9bd98a310a50ab1bb8334d8564c61961ebabdeb
mikero5 <michaelrobinson@netzero.net>
2019-08-31 12:42:10 -0600

    fix active record associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   validates :body, presence: true
 
-  belongs_to :User, optional: true
-  belongs_to :Post, optional: true
+  belongs_to :User, foreign_key: "user_id" #, optional: true
+  belongs_to :Post, foreign_key: "post_id" #, optional: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true
 
-  belongs_to :User  #, optional: true  # using optional true here, but shouldn't be needed
+  belongs_to :User, foreign_key: "user_id"  #, optional: true  # using optional true here, but shouldn't be needed
   has_many :Comments
 end
